993eb8793fe6a5387653034e0e54deb811f90855
ddipanfilo <ddipanfilo@gmail.com>
2016-10-07 20:21:36 -0400

    Change component name to NavBar

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import App from './app';
 import SessionFormContainer from './session_form/session_form_container';
 import PostIndexContainer from './post/post_index_container';
-import GreetingContainer from './greeting/greeting_container';
+import NavBar from './nav_bar/nav_bar_container';
 
 
 const Root = ({ store }) => {
