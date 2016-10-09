8a3bd6f7b3387cb901d281d253e296b74e138fa4
ddipanfilo <ddipanfilo@gmail.com>
2016-10-09 19:04:34 -0400

    Add styling to PostIndexItem

--- app/views/api/posts/_post.json.jbuilder
+++ app/views/api/posts/_post.json.jbuilder
@@ -1,2 +1,5 @@
-json.extract! post, :id, :user_id, :caption
+json.extract! post, :id, :user_id, :caption, :created_at
+json.user do
+  json.username post.user.username
+end
 json.image_url asset_url(post.image.url)
--- frontend/components/post_index_item/post_index_item.jsx
+++ frontend/components/post_index_item/post_index_item.jsx
@@ -7,12 +7,25 @@ class PostIndexItem extends React.Component {
   }
 
   render() {
+    let username = this.props.post.user.username.concat(" ");
+
     return(
       <div className="post-index-item">
+        <div className="post-index-item-header">
+          <h5 className="username-link">
+            { username }
+          </h5>
+        </div>
         <img className="post-picture"
           height="600"
           width="600"
           src={this.props.post.image_url} />
+        <div className="post-index-item-footer">
+          <div className="caption-holder">
+            <div className="caption-username">{ username } </div>
+            <div className="caption-text">{this.props.post.caption}</div>
+          </div>
+        </div>
       </div>
     );
   }
