a2740c81c708e77cf1b13f0a4d29330774cb7719
ddipanfilo <ddipanfilo@gmail.com>
2016-10-09 19:37:51 -0400

    Add time to Post Index Item

--- frontend/components/post_index_item/post_index_item.jsx
+++ frontend/components/post_index_item/post_index_item.jsx
@@ -4,6 +4,51 @@ import { hashHistory } from 'react-router';
 class PostIndexItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.timeSincePost = this.timeSincePost.bind(this);
+  }
+
+  timeSincePost(createdAt){
+    let currentTime = new Date();
+    let currentMinutes = currentTime.getUTCMinutes();
+    let currentHours = currentTime.getUTCHours();
+    let currentDate = currentTime.getUTCDate();
+    let currentMonth = currentTime.getUTCMonth();
+    let currentYear = currentTime.getUTCFullYear();
+
+
+    let postYear = createdAt.slice(0, 4);
+    let postMonth = createdAt.slice(5, 7);
+    let postDate = createdAt.slice(8, 10);
+    let postHours = createdAt.slice(11, 13);
+    let postMinutes = createdAt.slice(14, 16);
+    let postSeconds = createdAt.slice(17, 19);
+
+    let dateResult;
+
+    if (currentYear > postYear) {
+      dateResult = currentYear - postYear;
+      dateResult = dateResult + "y";
+    } else if (currentMonth > postMonth) {
+      dateResult = currentMonth - postMonth;
+      dateResult = dateResult + "m";
+    } else if(currentDate > postDate) {
+      dateResult = currentDate - postDate;
+      if (dateResult > 6) {
+        dateResult = Math.floor(dateResult / 7);
+        dateResult = dateResult + "w";
+      } else {
+      dateResult = dateResult + "d";
+      }
+    } else if (currentHours > postHours) {
+      dateResult = currentHours- postHours;
+      dateResult = dateResult + "h";
+    } else {
+      dateResult = currentMinutes- postMinutes;
+      dateResult = dateResult + "m";
+    }
+
+    return dateResult;
   }
 
   render() {
@@ -15,6 +60,9 @@ class PostIndexItem extends React.Component {
           <h5 className="username-link">
             { username }
           </h5>
+          <div className="post-time">
+            {this.timeSincePost(this.props.post.created_at)}
+          </div>
         </div>
         <img className="post-picture"
           height="600"
