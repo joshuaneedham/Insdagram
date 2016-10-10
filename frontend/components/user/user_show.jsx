e2912873939d840ffd37cdbd307cad9d4d0e33d1
ddipanfilo <ddipanfilo@gmail.com>
2016-10-10 17:15:32 -0400

    Style user show page

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -14,9 +14,23 @@ class UserShow extends React.Component {
   render(){
     if (this.props.user.posts) {
       const userPosts = this.props.user.posts.map (post =>
-        <img key={post.post_id} src={post.image_url} />
+        <img className="user-show-image" key={post.post_id} src={post.image_url} />
       );
-      return( <div>{ userPosts }</div> );
+      return(
+        <div className="user-show-container">
+          <div className="user-show-header">
+            <div className="user-show-information">
+              <div className="username-header">{this.props.user.username}</div>
+              <div className="user-stats">
+                <span className="posts-bold">{this.props.user.posts.length}</span>
+                <span className="posts-regular">posts</span>
+              </div>
+              <div className="user-full-name">{this.props.user.full_name}</div>
+            </div>
+          </div>
+          <div className="user-show-body">{ userPosts }</div>
+        </div>
+      );
     } else {
       return <div></div>;
     }
