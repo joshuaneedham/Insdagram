ade63c650b139cb54cf3efcef28580a5506a381c
ddipanfilo <ddipanfilo@gmail.com>
2016-11-22 15:50:39 -0500

    Render follow button

--- frontend/components/modal/post_show_modal.jsx
+++ frontend/components/modal/post_show_modal.jsx
@@ -6,10 +6,13 @@ import CommentForm from '../comment_form/comment_form_container';
 class PostShowModal extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { follow: 0 };
 
     this.likeText = this.likeText.bind(this);
     this.navigateUserShow = this.navigateUserShow.bind(this);
     this.likeAction = this.likeAction.bind(this);
+    this.checkUser = this.checkUser.bind(this);
+    this.followAction = this.followAction.bind(this);
   }
 
   componentDidMount(){
@@ -65,6 +68,61 @@ class PostShowModal extends React.Component {
     }
   }
 
+  checkUser(){
+    if (this.props.currentUser.id !== Object.values(this.props.post)[0].user_id) {
+      return(
+        <span className={ this.followCssClass() }
+          onClick={this.followAction}>{ this.followText() }
+        </span>
+      );
+    }
+  }
+
+  followStatus(){
+    for (var key in this.props.user.followers) {
+      if (this.props.user.followers[key].id === this.props.currentUser.id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  followText() {
+    if (this.followStatus()) {
+      return "Following";
+    } else {
+      return "Follow";
+    }
+  }
+
+  followCssClass(){
+    if (this.followStatus()) {
+      return "unfollowed-button";
+    } else {
+      return "followed-button";
+    }
+  }
+
+  followAction(){
+    this.setState({follow: this.state.follow + 1});
+
+    const userId = Object.values(this.props.post)[0].user_id;
+    if (this.followStatus()) {
+      this.props.destroyFollow(userId);
+    } else {
+      this.props.createFollow(userId);
+    }
+
+  }
+
+  followInformationText(){
+    if (this.props.user.followers.length === 1) {
+      return "follower";
+    } else {
+      return "followers";
+    }
+  }
+
   render(){
     if (Object.keys(this.props.post).length === 1) {
       let currentPost = Object.values(this.props.post)[0];
@@ -78,13 +136,13 @@ class PostShowModal extends React.Component {
           <div className="caption-text comment-text">{comment.body}</div>
         </div>
       );
-
       return (
         <div className="modal-content">
           <img className="modal-image" src={currentPost.image_url}/>
           <div className="modal-right">
-            <p className="modal-username"
-              onClick={() => this.navigateUserShow(currentPost.user_id)}>{currentPost.user.username}</p>
+            <span className="modal-username"
+              onClick={() => this.navigateUserShow(currentPost.user_id)}>{currentPost.user.username}</span>
+            { this.checkUser() }
             <div className="like-count">
               { currentPost.likes.length }
               { this.likeText() }
--- frontend/components/modal/post_show_modal_container.js
+++ frontend/components/modal/post_show_modal_container.js
@@ -2,16 +2,20 @@ import { connect } from 'react-redux';
 import PostShowModal from './post_show_modal';
 import { createLike, destroyLike } from '../../actions/like_actions';
 import { requestPost } from '../../actions/post_actions';
+import { createFollow, destroyFollow } from '../../actions/follow_actions';
 
 const mapStateToProps = (state, ownProps) => ({
   currentUser: state.session.currentUser,
-  post: state.posts
+  post: state.posts,
+  user: state.user
 });
 
 const mapDispatchToProps = dispatch => ({
   requestPost: id => dispatch(requestPost(id)),
   createLike: postId => dispatch(createLike(postId)),
-  destroyLike: postId => dispatch(destroyLike(postId))
+  destroyLike: postId => dispatch(destroyLike(postId)),
+  createFollow: userId => dispatch(createFollow(userId)),
+  destroyFollow: userId => dispatch(destroyFollow(userId))
 });
 
 export default connect(
