b5840a305f359c8dfa0303827c649d927c23139f
ddipanfilo <ddipanfilo@gmail.com>
2016-12-07 23:30:30 -0500

    Make style consistent

--- frontend/components/suggested_users/suggested_users_container.js
+++ frontend/components/suggested_users/suggested_users_container.js
@@ -3,16 +3,15 @@ import SuggestedUsers from './suggested_users';
 import { createFollow, destroyFollow } from '../../actions/follow_actions';
 import { requestPosts } from '../../actions/post_actions';
 
-const mapStateToProps = (state) => {
-  return ({
-    suggestedUsers: (state.suggestedUsers),
-    currentUser: state.session.currentUser
-  });
-};
+
+const mapStateToProps = (state) => ({
+    suggestedUsers: state.suggestedUsers,
+    currentUser: state.session.currentUser,
+});
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   createFollow: userId => dispatch(createFollow(userId)),
-  destroyFollow: userId => dispatch(destroyFollow(userId))
+  destroyFollow: userId => dispatch(destroyFollow(userId)),
 });
 
 export default connect(
