ea878ab6534e061aa9a600fe8ae2cc03eae5cc84
ddipanfilo <ddipanfilo@gmail.com>
2016-12-08 14:13:06 -0500

    Fix unfollow bug

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -34,7 +34,7 @@ class Api::UsersController < ApplicationController
 
   def unfollow
     @user = User.find(params[:id])
-    follow = Follow.find_by(followed_id: current_user.id, followed_id: @user.id)
+    follow = Follow.find_by(follower_id: current_user.id, followed_id: @user.id)
     if follow.destroy
       render :show
     else
